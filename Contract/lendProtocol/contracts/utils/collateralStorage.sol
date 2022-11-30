0c2948e6644f7c33f90f67e963ae880ada14e3cf
kayzee <olaniyikazeem112@gmail.com>
2022-11-30 01:55:04 +0100

    implemented collateral and lendareserve

--- Contract/lendProtocol/contracts/utils/collateralStorage.sol
+++ Contract/lendProtocol/contracts/utils/collateralStorage.sol
@@ -2,7 +2,30 @@
 
 pragma solidity 0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {ICollateralStorage} from "../interfaces/ICollateralStorage.sol";
+
 /** @title Collateral Storage contract
     @dev This contract is responsible for holding borrowers collateral
 **/
-contract CollateralStorage {}
+contract CollateralStorage is ICollateralStorage{
+/***==========================
+    =   Error Message        =
+    ==========================*/
+    error mustApprove(string);
+
+
+    function depositToStorage(address _depositor, uint256 _tokenId, address _nftContractAddress) external override {
+        IERC721 nftContractAddress = IERC721(_nftContractAddress);
+        if(nftContractAddress.isApprovedForAll(_depositor, address(this)) != true) revert mustApprove("must approve to spend");
+        nftContractAddress.transferFrom(_depositor, address(this), _tokenId);
+    }
+
+    function withdrawFromStorage(address _depositor, uint256 _tokenId, address _nftContractAddress) external override {
+        IERC721(_nftContractAddress).transferFrom(address(this), _depositor, _tokenId);
+    }
+
+    function valueInReserve(address _nftContractAddress) external view override returns(uint256) {
+        return IERC721(_nftContractAddress).balanceOf(address(this));
+    }
+}
--- Contract/lendProtocol/contracts/utils/lendaReserve.sol
+++ Contract/lendProtocol/contracts/utils/lendaReserve.sol
@@ -2,7 +2,59 @@
 
 pragma solidity 0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ILendaReserve} from "../interfaces/ILendaReserve.sol";
+
 /** @title Lenda Matic Reserve
     @dev This contract is responsible for holding matic deposit of depositors/lenders
 **/
-contract LendaReserve {}
+contract LendaReserve is ILendaReserve {
+/***==========================
+    =   Error Message        =
+    ==========================*/
+    error mustApprove(string);
+
+
+
+    IERC20 maticContractAddress;
+    address admin;
+
+
+    modifier onlyOwner(){
+        require(msg.sender == admin, "Only admin can call this function");
+
+        _;
+    }
+
+    constructor(address _maticContractAddress){
+        maticContractAddress = IERC20(_maticContractAddress);
+    }
+
+    function depositToReserve(uint256 _amount, address _depositor) external override{
+        if(maticContractAddress.allowance(_depositor, address(this)) >= _amount) revert mustApprove("amount to deposit must be approved");
+        maticContractAddress.transferFrom(_depositor, address(this), _amount);
+    }
+
+    function withdrawFromReserve(uint256 _amount, address _depositor) external override{
+        maticContractAddress.transfer(_depositor, _amount);
+    }
+
+    function updateContractAddress(address _maticContractAddress) external override onlyOwner{
+        maticContractAddress = IERC20(_maticContractAddress);
+    }
+
+    function movingGeneric(address _receiver, address _tokenContractAddress, uint256 _amount) external override onlyOwner{
+        IERC20(_tokenContractAddress).transfer(_receiver, _amount);
+    }
+
+    function updateAdmin(address _newAdmin) external override onlyOwner{
+        admin = _newAdmin;
+    }
+
+    //View functions
+
+    function valueInReserve() external override view returns(uint256) {
+        return maticContractAddress.balanceOf(address(this));
+    }
+
+}
