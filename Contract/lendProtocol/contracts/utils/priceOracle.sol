3aecb2568386f91a04a2a45e563edda6a8bd9a02
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-11-30 21:16:02 +0100

    price oracle

--- Contract/lendProtocol/contracts/utils/priceOracle.sol
+++ Contract/lendProtocol/contracts/utils/priceOracle.sol
@@ -1,9 +1,44 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.9;
+pragma solidity 0.8.17;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /** @title Price Oracle Contract
     @dev This contract aims to get the onchain data of NFT floor price across all NFT platforms
     - to deduce Lenda NFT worth
 **/
-contract PriceOracle {}
+contract PriceOracle {
+
+     AggregatorV3Interface internal nftFloorPriceFeed;
+
+    /**
+     * Network: Goerli
+     * Aggregator: CryptoPunks
+     * Address: 0x5c13b249846540F81c093Bc342b5d963a7518145
+     */
+    constructor() {
+        nftFloorPriceFeed = AggregatorV3Interface(
+            0x5c13b249846540F81c093Bc342b5d963a7518145
+        );
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int) {
+        (
+            ,
+            /*uint80 roundID*/ 
+            int nftFloorPrice 
+            /*uint startedAt*/ 
+            /*uint timeStamp*/ 
+            /*uint80 answeredInRound*/,
+            ,
+            ,
+
+        ) = nftFloorPriceFeed.latestRoundData();
+        return nftFloorPrice;
+    }
+
+}
