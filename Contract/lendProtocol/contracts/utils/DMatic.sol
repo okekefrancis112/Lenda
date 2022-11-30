3a661df8f4b59b296275ac01f0c691786ac3fa18
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-11-30 15:52:23 +0100

    first commit

--- Contract/lendProtocol/contracts/utils/DMatic.sol
+++ Contract/lendProtocol/contracts/utils/DMatic.sol
@@ -2,6 +2,9 @@
 
 pragma solidity 0.8.9;
 
+import {IERC20} from "../interfaces/IERC20.sol";
+import {IERC721} from "../interfaces/IERC721.sol";
+
 /** @title Depositor Token
     @dev this token is pegged to the amount a lender deposit at a ratio 1:1
     @dev Depositor can then lock this DMatic token to yield some LND token (Lenda token) as an incentive for them
