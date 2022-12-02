bc7fc71cb12103a2308655087e5f4ef32f356f47
kayzee <olaniyikazeem112@gmail.com>
2022-12-02 11:49:26 +0100

    modified collateral storage contract

--- Contract/lendProtocol/contracts/libraries/collateralStorage.sol
+++ Contract/lendProtocol/contracts/libraries/collateralStorage.sol
@@ -3,29 +3,28 @@
 pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {ICollateralStorage} from "../interfaces/ICollateralStorage.sol";
 
 /** @title Collateral Storage contract
     @dev This contract is responsible for holding borrowers collateral
 **/
-contract CollateralStorage is ICollateralStorage{
+library CollateralStorage{
 /***==========================
     =   Error Message        =
     ==========================*/
     error mustApprove(string);
 
 
-    function depositToStorage(address _depositor, uint256 _tokenId, address _nftContractAddress) external override {
+    function depositToStorage(address _depositor, uint256 _tokenId, address _nftContractAddress) internal {
         IERC721 nftContractAddress = IERC721(_nftContractAddress);
         if(nftContractAddress.isApprovedForAll(_depositor, address(this)) != true) revert mustApprove("must approve to spend");
         nftContractAddress.transferFrom(_depositor, address(this), _tokenId);
     }
 
-    function withdrawFromStorage(address _depositor, uint256 _tokenId, address _nftContractAddress) external override {
+    function withdrawFromStorage(address _depositor, uint256 _tokenId, address _nftContractAddress) internal {
         IERC721(_nftContractAddress).transferFrom(address(this), _depositor, _tokenId);
     }
 
-    function valueInReserve(address _nftContractAddress) external view override returns(uint256) {
+    function valueInReserve(address _nftContractAddress) public view returns(uint256) {
         return IERC721(_nftContractAddress).balanceOf(address(this));
     }
 }
