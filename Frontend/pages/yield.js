05918e9c553f2cca2b494fa9e39e3c810ff3e8e8
realsbd <abimbola.salawudeen@gmail.com>
2022-12-04 07:38:56 +0800

    giving abi.map is not a function error

--- Frontend/pages/yield.js
+++ Frontend/pages/yield.js
@@ -35,14 +35,28 @@ export default function Yield() {
     args: [tokenAmount, lockDays],
   });
 
+  const {
+    data,
+    isSuccess,
+    isLoading,
+    write: withdrawToken
+  } = useContractWrite({
+        mode: "recklesslyUnprepared",
+        ...YIELD_ADDRESS,
+        functionName: "withdrawLockedToken",
+        args:[]
+      }
+
+  )
+
   // const { isLoading: lockDtokenLoading } = useWaitForTransaction(
   //     hash: lockDtoken?.hash,
   //     onSuccess()
   // )
 
-  const handleLock = async () =>{
-    lockDmatic()
-  }
+  // const handleLock = async () =>{
+  //   lockDmatic()
+  // }
 
   return (
     <div>
@@ -61,7 +75,7 @@ export default function Yield() {
             <span className="text-[20px]">0</span>
           </div>
         </div>
-        <div className="mb-20 max-w-[800px] mx-auto px-10 py-10 bg-[#33315c] rounded-md">
+        <div className="mb-20 max-w-[600px] mx-auto px-10 py-10 bg-[#33315c] rounded-md">
           <h3 className="font-bold text-2xl mb-4 text-white]">USER DETAILS</h3>
           <div className="flex gap-5">
             <div className="bg-[#cb2b83] w-1/2 px-6 pt-3 pb-10">
@@ -90,9 +104,7 @@ export default function Yield() {
                       Lock Dmatic Token
                     </h3>
                     <button
-                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                        onClick={() => setLockDmaticModal(false)}
-                    >
+                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none">
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
@@ -119,56 +131,23 @@ export default function Yield() {
                       <button
                           className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                           type="button"
+                          onClick={() => lockDmatic?.()}
                       >
                         Lock Token
                       </button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
 
-            <div className="justify-center items-center flex">
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                {/*content*/}
-                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                  {/*header*/}
-                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <h3 className="text-3xl font-semibold text-black">
-                      Withdraw Token
-                    </h3>
-                    <button
-                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-
-                    >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                      ×
-                    </span>
-                    </button>
-                  </div>
-                  <form className={styles.form}>
-                    {/*body*/}
-                    <div className="relative p-6 flex-auto">
-                      <input
-                          className={styles.input}
-                          placeholder="Amount"
-                          value=""
-                      />
-                    </div>
-                    {/*footer*/}
-                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                       <button
-                          className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          className="bg-amber-700 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                           type="button"
+                          onClick={() => withdrawToken?.()}
                       >
-                        Lock Token
+                        Withdraw Token
                       </button>
                     </div>
                   </form>
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </div>
