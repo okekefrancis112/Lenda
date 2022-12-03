5a3adec9465ae14116d1c561ca9894dc91012bbc
realsbd <abimbola.salawudeen@gmail.com>
2022-12-04 07:07:18 +0800

    lock token set

--- Frontend/pages/yield.js
+++ Frontend/pages/yield.js
@@ -1,4 +1,49 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/Borrow.module.css";
+
+import {
+  useAccount,
+  useConnect,
+  useContractWrite,
+  useDisconnect,
+  useContractRead, useWaitForTransaction,
+} from "wagmi";
+
+import { InjectedConnector } from "wagmi/connectors/injected";
+import {YIELD_ADDRESS} from "../utils/index";
+import axios from "axios";
+import {ethers} from "ethers";
+
 export default function Yield() {
+  const [ lockDmaticModal, setLockDmaticModal] = useState(false)
+
+  const [tokenAmount, setTokenAmount] = useState()
+  const [lockDays, setLockDays] = useState()
+  const [loading, setLoading]  = useState()
+  const { address, isDisconnected, isConnected } = useAccount()
+
+  const {
+    data: lockDtoken,
+    isError,
+    isSuccess: lockDtokenSuccess,
+    isLoading: loadinglockDtoken,
+      write: lockDmatic
+  } = useContractWrite({
+    mode: "recklesslyUnprepared",
+    ...YIELD_ADDRESS,
+    functionName: "lockDTokenForLND",
+    args: [tokenAmount, lockDays],
+  });
+
+  // const { isLoading: lockDtokenLoading } = useWaitForTransaction(
+  //     hash: lockDtoken?.hash,
+  //     onSuccess()
+  // )
+
+  const handleLock = async () =>{
+    lockDmatic()
+  }
+
   return (
     <div>
       <div className="max-w-[1300px] mx-auto text-white px-10 mt-10">
@@ -16,7 +61,7 @@ export default function Yield() {
             <span className="text-[20px]">0</span>
           </div>
         </div>
-        <div className="mb-20 max-w-[600px] mx-auto px-10 py-10 bg-[#33315c] rounded-md">
+        <div className="mb-20 max-w-[800px] mx-auto px-10 py-10 bg-[#33315c] rounded-md">
           <h3 className="font-bold text-2xl mb-4 text-white]">USER DETAILS</h3>
           <div className="flex gap-5">
             <div className="bg-[#cb2b83] w-1/2 px-6 pt-3 pb-10">
@@ -35,9 +80,95 @@ export default function Yield() {
           </div>
 
           <div className="flex mx-auto">
-            <button className="bg-white px-16 py-2 mx-auto text-[#000] mt-5">
-              Lock MATIC
-            </button>
+            <div className="justify-center items-center flex">
+              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                    <h3 className="text-3xl font-semibold text-black">
+                      Lock Dmatic Token
+                    </h3>
+                    <button
+                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                        onClick={() => setLockDmaticModal(false)}
+                    >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                    </button>
+                  </div>
+                  <form className={styles.form}>
+                    {/*body*/}
+                    <div className="relative p-6 flex-auto">
+                      <input
+                          className={styles.input}
+                          placeholder="Amount"
+                          value={tokenAmount}
+                          onChange={(e) => setTokenAmount(e.target.value)}
+                      />
+                      <input
+                          className={styles.input}
+                          placeholder="No of Days"
+                          value={lockDays}
+                          onChange={(e) => setLockDays(e.target.value)}
+                      />
+                    </div>
+                    {/*footer*/}
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                          className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                      >
+                        Lock Token
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            <div className="justify-center items-center flex">
+              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                    <h3 className="text-3xl font-semibold text-black">
+                      Withdraw Token
+                    </h3>
+                    <button
+                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+
+                    >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                    </button>
+                  </div>
+                  <form className={styles.form}>
+                    {/*body*/}
+                    <div className="relative p-6 flex-auto">
+                      <input
+                          className={styles.input}
+                          placeholder="Amount"
+                          value=""
+                      />
+                    </div>
+                    {/*footer*/}
+                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                      <button
+                          className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                      >
+                        Lock Token
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
--- Frontend/utils/index.js
+++ Frontend/utils/index.js
@@ -1,8 +1,11 @@
 import lendPool from "../utils/abi/lendpoolabi.json";
 import mMaticToken from "../utils/abi/mMatic.json";
+import yieldAbi from "./abi/yieldAbi.json";
+
 
 export const LENDPOOL_ADDRESS = "0x24F1D4ed95217C41639FB8C6951bf6aBd6cB5361";
 export const mMATIC_ADDRESS = "0x74d68b3c83ED72Da304dc4b7F8A25696CC11411a";
+export const YIELDCONTRACT_ADDRESS = "0xCA3529CD293BF6B3d604193F36f9604f91D11d5c";
 
 export const LENDA_CONTRACT = {
   address: LENDPOOL_ADDRESS,
@@ -13,3 +16,8 @@ export const mMATIC_CONTRACT = {
   address: mMATIC_ADDRESS,
   abi: mMaticToken.abi,
 };
+
+export const YIELD_ADDRESS = {
+  address: YIELDCONTRACT_ADDRESS,
+  abi: yieldAbi,
+}
